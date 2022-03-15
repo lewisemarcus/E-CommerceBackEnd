273cf49b6d0ca64e8e2a475910fbbfe19bbd0ea1
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-03-15 16:20:38 -0700

    updated category-routes DELETE function to return json response

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -83,6 +83,11 @@ router.delete('/:id', async (req, res) => {
     const categoryData = await Category.destroy({ where: { id: req.params.id } })
 
     if (!categoryData) return res.status(404).json({ message: 'No category found with this id!' })
+    else {
+      res.status(200).json({
+        message: `Category DELETED`,
+        body: categoryData})
+    }
   }
   catch (err) {
     res.status(500).json({
